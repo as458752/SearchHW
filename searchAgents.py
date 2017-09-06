b85508f810876b6651f68a2ab432f2a3963f255f
Habib Matar <habib.matar@gmail.com>
2017-09-06 13:35:54 -0700

    Add files via upload

--- searchAgents.py
+++ searchAgents.py
@@ -287,22 +287,23 @@ class CornersProblem(search.SearchProblem):
         self._expanded = 0 # DO NOT CHANGE; Number of search nodes expanded
         # Please add any code here which you would like to use
         # in initializing the problem
-        "*** YOUR CODE HERE ***"
+        
+        self.startState = (self.startingPosition , self.corners)
 
     def getStartState(self):
         """
         Returns the start state (in your state space, not the full Pacman state
         space)
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.startState
 
     def isGoalState(self, state):
         """
         Returns whether this search state is a goal state of the problem.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if len(state[1]) == 0:
+            return True
+        return False
 
     def getSuccessors(self, state):
         """
@@ -319,12 +320,21 @@ class CornersProblem(search.SearchProblem):
         for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
             # Add a successor state to the successor list if the action is legal
             # Here's a code snippet for figuring out whether a new position hits a wall:
-            #   x,y = currentPosition
-            #   dx, dy = Actions.directionToVector(action)
-            #   nextx, nexty = int(x + dx), int(y + dy)
-            #   hitsWall = self.walls[nextx][nexty]
+            x,y = state[0][0], state[0][1]
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            hitsWall = self.walls[nextx][nexty]
 
-            "*** YOUR CODE HERE ***"
+            if not hitsWall:
+                cornersLeft = state[1][:]
+                nextAction = (nextx, nexty)
+
+                if nextAction in cornersLeft:
+                    tempList = list(cornersLeft)
+                    tempList.remove(nextAction)
+                    cornersLeft = tuple(tempList)
+                
+                successors.append(((nextAction, cornersLeft), action, 1))
 
         self._expanded += 1 # DO NOT CHANGE
         return successors
